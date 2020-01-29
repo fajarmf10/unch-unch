52e98ebf9d45731099339b2db9db9d988be1ace6
Fajar Maulana Firdaus <Fajar.Firdaus@btpn.com>
2020-01-29 07:26:23 +0700

    [Fajar] Initial Commit

---
+++ src/main/java/com/fajarmf/demoaja/DemoajaApplication.java
@@ -0,0 +1,13 @@
+package com.fajarmf.demoaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoajaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fajarmf/demoaja/DemoajaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fajarmf.demoaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
