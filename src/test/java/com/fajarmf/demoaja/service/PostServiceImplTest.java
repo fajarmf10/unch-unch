608f5a62a3ea67f3858f3dad6804ec049d5dd9e1
Fajar Maulana Firdaus <Fajar.Firdaus@btpn.com>
2020-01-29 11:42:24 +0700

    [Fajar] Adds annotation @DisplayName to display test description

--- src/test/java/com/fajarmf/demoaja/service/PostServiceImplTest.java
+++ src/test/java/com/fajarmf/demoaja/service/PostServiceImplTest.java
@@ -2,11 +2,15 @@ package com.fajarmf.demoaja.service;
 
 import com.fajarmf.demoaja.entity.Post;
 import com.fajarmf.demoaja.repository.PostRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.event.annotation.BeforeTestExecution;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,20 +28,35 @@ class PostServiceImplTest {
     @InjectMocks
     PostServiceImpl service;
 
-    @Test
-    void getAllPosts_shouldSuccess_whenThereIsAtLeastOnePost() {
-        List<Post> postList = new ArrayList<>();
-        Post firstPost = new Post();
+    private List<Post> postList;
+    private Post firstPost;
+    private Post secondPost;
+
+    @BeforeTestExecution
+    public void setUp() {
+        postList = new ArrayList<>();
+        firstPost = new Post();
         firstPost.setTitle("Test");
-        firstPost.setSlug("/test-post");
+        firstPost.setSlug("test-post");
         firstPost.setBody("Hari ini aku senang sekali bisa bertemu denganmu</br>Karena kamu ada di sisiku <3</br>Kamu adalah matahariku <3</br>");
+        secondPost = new Post();
+        secondPost.setTitle("Second Post");
+        secondPost.setSlug("second-post-incoming");
+        secondPost.setBody("This is just an example</br>Just open your laptop and do your way");
         postList.add(firstPost);
+        postList.add(secondPost);
+    }
+
+    @Test
+    @DisplayName("Should get all Posts")
+    void getAllPosts_shouldSuccess_whenThereIsAtLeastOnePost() {
         when(repository.findAll()).thenReturn(postList);
         List<Post> result = service.getAllPosts();
         assertEquals(postList, result);
     }
 
     @Test
+    @DisplayName("Should throw the error")
     void getAllPosts_shouldThrow_whenThereIsNoPost() {
         when(repository.findAll()).thenThrow(new EmptyResultDataAccessException("Alah ngethow mulu", 1));
         assertThrows(EmptyResultDataAccessException.class, () -> {
@@ -46,7 +65,11 @@ class PostServiceImplTest {
     }
 
     @Test
-    void getPostUsingSlug() {
+    @DisplayName("Should return the wanted post")
+    void getPostUsingSlug_shouldReturnTheWantedPost_whenTheSlugIsCorrect() {
+        when(repository.getPostBySlug("second-post-incoming")).thenReturn(secondPost);
+        Post result = service.getPostUsingSlug("second-post-incoming");
+        assertEquals(secondPost, result);
     }
 
     @Test
